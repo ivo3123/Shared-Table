967ea153083d8634955812a0def0f0f6562f3dca
Dimitar Tankov <dimitar.tankov@sap.com>
2025-05-09 17:03:56 +0300

    Add shared table class with separate rendering

---
+++ js/shared-table.js
@@ -0,0 +1,81 @@
+class SharedTable {
+    static #DEFAULT_ROWS = 10;
+    static #DEFAULT_COLUMNS = 27;
+    static #DEFAULT_CELL_VALUE = "";
+    #cells = [];
+    #element = null;
+
+    static defaultTableData() {
+        return Array.from({ length: SharedTable.#DEFAULT_ROWS }, () =>
+            Array.from({ length: SharedTable.#DEFAULT_COLUMNS }, () => SharedTable.#DEFAULT_CELL_VALUE)
+        );
+    }
+
+    static getHeaderLetter(index) {
+        let letter = "";
+        index += 1;
+
+        while (index > 0) {
+            index -= 1;
+            letter += String.fromCharCode(65 + index % 26);
+            index = Math.floor(index / 26);
+        }
+
+        return letter;
+    }
+
+    constructor() {
+        this.#cells = SharedTable.defaultTableData();
+    }
+
+    #renderHeader() {
+        const headerRow = document.createElement("tr");
+
+        headerRow.appendChild(document.createElement("th"));
+        for (let i = 0; i < this.#cells[0].length; ++i) {
+            const th = document.createElement("th");
+            th.textContent = SharedTable.getHeaderLetter(i);
+            headerRow.appendChild(th);
+        }
+
+        return headerRow;
+    }
+
+    #renderRow(rowIndex) {
+        const row = document.createElement("tr");
+
+        const rowLabel = document.createElement("th");
+        rowLabel.textContent = rowIndex + 1;
+        row.appendChild(rowLabel);
+
+        for (let i = 0; i < this.#cells[rowIndex].length; ++i) {
+            const cell = document.createElement("td");
+            cell.contentEditable = "true";
+            cell.dataset.cell = `${SharedTable.getHeaderLetter(i)}${rowIndex + 1}`;
+            cell.textContent = this.#cells[rowIndex][i];
+            row.appendChild(cell);
+        }
+
+        return row;
+    }
+
+    render() {
+        const table = document.createElement("table");
+        const thead = document.createElement("thead");
+        const tbody = document.createElement("tbody");
+
+        thead.appendChild(this.#renderHeader());
+        this.#cells.forEach((_, index) => {
+            tbody.appendChild(this.#renderRow(index));
+        });
+
+        table.appendChild(thead);
+        table.appendChild(tbody);
+
+        this.#element = table;
+
+        return table;
+    }
+}
+
+export default SharedTable;
