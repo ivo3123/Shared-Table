30511377efaa615a1238282812d40228b086b46c
Dimitar Tankov <dimitar.tankov@sap.com>
2025-05-12 23:00:30 +0300

    Refactor blocking evaluator class

--- js/blocking-evaluator.js
+++ js/blocking-evaluator.js
@@ -1,22 +1,9 @@
+import { showError, clearError } from "./util.js";
+
 class BlockingEvaluator {
     static #DEFAULT_BLOCKING_FUNCTION = (row, col) => false;
     static BLOCKING_STATEMENT_CHANGE_EVENT = 'blockingStatementChange';
     #blockingFunction = null;
-
-    static #showError(inputElement, message) {
-        const errorDiv = document.createElement('div');
-        errorDiv.className = 'error';
-        errorDiv.textContent = message;
-
-        inputElement.insertAdjacentElement('afterend', errorDiv);
-    }
-
-    static #clearError(inputElement) {
-        const errorDiv = inputElement.nextElementSibling;
-        if (errorDiv && errorDiv.classList.contains('error')) {
-            errorDiv.remove();
-        }
-    }
 
     constructor() {
         this.#blockingFunction = BlockingEvaluator.#DEFAULT_BLOCKING_FUNCTION;
@@ -32,35 +19,32 @@ class BlockingEvaluator {
             this.#blockingFunction = blockingFunction;
         }
         catch (error) {
-            BlockingEvaluator.#showError(input, 'Invalid blocking statement');
+            showError(input, 'Invalid blocking statement');
         }
     }
 
-    #renderBlockingStatementHtml() {
-        const fieldDiv = document.createElement('div');
-        fieldDiv.className = 'field';
-
+    render(blockingEvaluatorContainer) {
         const label = document.createElement('label');
         const strong = document.createElement('strong');
         strong.textContent = 'Enter blocking statement (row, col):';
         label.appendChild(strong);
-        fieldDiv.appendChild(label);
+        blockingEvaluatorContainer.appendChild(label);
 
-        fieldDiv.appendChild(document.createElement('br'));
+        blockingEvaluatorContainer.appendChild(document.createElement('br'));
 
         const input = document.createElement('input');
         input.type = 'text';
         input.id = 'blocking_statement';
         input.style.width = '100%';
         input.placeholder = 'row % 2 === 0 && col % 2 === 0';
-        fieldDiv.appendChild(input);
+        blockingEvaluatorContainer.appendChild(input);
 
         input.addEventListener('change', (event) => {
             const blockingStatement = event.target.value;
             if (!blockingStatement) {
                 return;
             }
-            BlockingEvaluator.#clearError(input);
+            clearError(input);
 
             this.#setBlockingFunction(blockingStatement);
             const newEvent = new Event(BlockingEvaluator.BLOCKING_STATEMENT_CHANGE_EVENT, { bubbles: true });
@@ -72,24 +56,12 @@ class BlockingEvaluator {
                 return;
             }
             const blockingStatement = event.target.value;
-            BlockingEvaluator.#clearError(input);
+            clearError(input);
 
             this.#setBlockingFunction(blockingStatement);
             const newEvent = new Event(BlockingEvaluator.BLOCKING_STATEMENT_CHANGE_EVENT, { bubbles: true });
             input.dispatchEvent(newEvent);
         });
-
-        return fieldDiv;
-    }
-
-    render() {
-        const container = document.createElement('div');
-        container.className = 'blocking-evaluator';
-
-        const blockingStatementHtml = this.#renderBlockingStatementHtml();
-        container.appendChild(blockingStatementHtml);
-
-        return container;
     }
 }
 
